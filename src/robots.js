0d3371164a35d4b73d529acfbc4bbec0628cfaf8
Garrett <gkatashima@gmail.com>
2019-02-06 15:15:14 -0800

    created app.js and index.js

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Steph Curry',
+    username: 'Steph',
+    email: 'Curry@curry.com'
+  },
+  {
+    id: 2,
+    name: 'Lebron James',
+    username: 'Lebron',
+    email: 'lbj@lebron.com'
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Allen Iverson',
+    username: 'AI',
+    email: 'allen@allen.com'
+  },
+];
